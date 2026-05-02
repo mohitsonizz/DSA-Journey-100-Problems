8f9990841318a04dc283d517b6cd30dc391be028
mohitsonizz <mohitsoni.education@gmail.com>
2026-05-02 19:11:40 +0530

    Even_Odd

--- 01-Basic-Programming/Numbers/01_Even_Odd.py
+++ 01-Basic-Programming/Numbers/01_Even_Odd.py
@@ -1,1 +1,5 @@
-
+def isEven(n:int) -> bool:
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer.")
+    # "Even" if n%2==0 else "Odd"   -  Slightly Slower
+    return (n&1)==0  # Extremely Fast (CPU level)
