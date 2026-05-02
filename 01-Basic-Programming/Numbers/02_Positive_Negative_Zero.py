02082a019d677b9d183e7391021b59f2f982c659
mohitsonizz <mohitsoni.education@gmail.com>
2026-05-02 19:30:53 +0530

    Positive_Negative_Zero

--- 01-Basic-Programming/Numbers/02_Positive_Negative_Zero.py
+++ 01-Basic-Programming/Numbers/02_Positive_Negative_Zero.py
@@ -1,1 +1,10 @@
-.
+def check_number_status(n: float) -> str:
+    if not isinstance(n, (int, float)):
+        raise ValueError("Input must be a numeric value (int or float).")
+
+    if n > 0:
+        return "Positive"
+    elif n < 0:
+        return "Negative"
+    else:
+        return "Zero".
