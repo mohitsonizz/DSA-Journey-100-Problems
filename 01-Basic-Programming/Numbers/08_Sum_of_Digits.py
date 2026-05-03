157492d32eee70ab4c6b4faea15da19d115fe4a9
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-03 13:23:21 +0530

    Sum_of_Digits

--- 01-Basic-Programming/Numbers/08_Sum_of_Digits.py
+++ 01-Basic-Programming/Numbers/08_Sum_of_Digits.py
@@ -1,1 +1,10 @@
+def sum_of_digits(n):
+    n = abs(n)
+    total_sum = 0
+    while n > 0:
+        digit = n % 10      
+        total_sum += digit 
+        n //= 10            
+      
+    return total_sum
 
