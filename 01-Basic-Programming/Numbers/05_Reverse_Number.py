77391c4b8de7997f1f0359ebb9a97c2f3e042d99
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-03 09:28:54 +0530

    Reverse_Number

--- 01-Basic-Programming/Numbers/05_Reverse_Number.py
+++ 01-Basic-Programming/Numbers/05_Reverse_Number.py
@@ -1,1 +1,21 @@
+def reverse_number(n):
+    is_negative = n < 0
+    num = abs(n)
+    
+    reversed_num = 0
+    while num > 0:
+        
+        last_digit = num % 10
+        reversed_num = (reversed_num * 10) + last_digit
+        num //= 10
+        
+    return -reversed_num if is_negative else reversed_num
 
+''' 500 --> 005  
+def reverse_string_way(n):
+    s = str(n)
+    if s[0] == '-':
+        return int('-' + s[1:][::-1])
+    return int(s[::-1
+'''    
+    
