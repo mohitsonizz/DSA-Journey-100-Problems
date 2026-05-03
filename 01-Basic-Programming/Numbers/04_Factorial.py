20165a45c5e2312cbca3aa07cdf1973e0431c137
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-03 09:13:04 +0530

    Factorial

--- 01-Basic-Programming/Numbers/04_Factorial.py
+++ 01-Basic-Programming/Numbers/04_Factorial.py
@@ -1,1 +1,10 @@
-
+def get_factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n == 0 or n == 1:
+        return 1
+    
+    num = 1
+    for i in range(2, n + 1):
+        num *= i
+    return num
