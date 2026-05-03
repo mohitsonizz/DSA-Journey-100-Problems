31fd02a5f3e406a008cdbe6061e0aac26b6b7bf6
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-03 11:02:13 +0530

    Armstrong_Number

--- 01-Basic-Programming/Numbers/07_Armstrong_Number.py
+++ 01-Basic-Programming/Numbers/07_Armstrong_Number.py
@@ -1,1 +1,14 @@
+def is_armstrong(n):
+    if n < 0:
+        return False
 
+    power = len(str(num_str))
+    total_sum = 0
+    temp = n
+    
+    while temp_n > 0:
+        digit = temp % 10
+        total_sum += digit ** power
+        temp //= 10
+        
+    return total_sum == n
