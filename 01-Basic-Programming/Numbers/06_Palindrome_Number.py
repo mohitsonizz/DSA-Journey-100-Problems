dd66e4241846a26b63ad7e5fa05f81ecaaa61031
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-03 10:41:15 +0530

    Palindrome_Number

--- 01-Basic-Programming/Numbers/06_Palindrome_Number.py
+++ 01-Basic-Programming/Numbers/06_Palindrome_Number.py
@@ -1,1 +1,12 @@
+def is_palindrome(n):
+    # Basic Checks
+    if n < 0 or (n % 10 == 0 and n != 0):
+        return False
 
+    rev = 0
+    while n > rev:
+        last_digit = n % 10
+        rev = (rev * 10) + last_digit
+        n = n // 10  # Number ko chhota karte jao
+
+    return n == rev or n == rev // 10 
