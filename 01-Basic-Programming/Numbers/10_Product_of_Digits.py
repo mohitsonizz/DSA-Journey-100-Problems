0836e26ea89f266d40c76a5cc58b22728deb83fa
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 17:54:13 +0530

    Product_of_Digits

--- 01-Basic-Programming/Numbers/10_Product_of_Digits.py
+++ 01-Basic-Programming/Numbers/10_Product_of_Digits.py
@@ -1,1 +1,17 @@
-
+def get_product_of_digits(n: int) -> int:
+    n = abs(n)
+    
+    if n == 0:
+        return 0
+    
+    product = 1
+    
+    while n > 0:
+        digit = n % 10
+        # Update product
+        product *= digit
+        if product == 0:
+            return 0
+        n //= 10
+        
+    return product
