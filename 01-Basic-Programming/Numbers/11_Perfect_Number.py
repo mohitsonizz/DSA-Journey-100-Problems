1ebc8fdb4da5894bf119b2bd82a81437118b2996
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 18:10:24 +0530

    Perfect_Number

--- 01-Basic-Programming/Numbers/11_Perfect_Number.py
+++ 01-Basic-Programming/Numbers/11_Perfect_Number.py
@@ -1,1 +1,15 @@
-
+def check_perfect_number(n):
+    if n <= 1:
+        return False
+    
+    div_sum = 1
+    
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            div_sum += i
+            if i * i != n:
+                div_sum += n // i
+        i += 1
+        
+    return div_sum == n
