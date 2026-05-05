d8a4bca141afda7286023de05f9fe4db12bae18f
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 19:07:15 +0530

    Automorphic_Number

--- 01-Basic-Programming/Numbers/13_Automorphic_Number.py
+++ 01-Basic-Programming/Numbers/13_Automorphic_Number.py
@@ -1,1 +1,17 @@
-
+def is_automorphic(n):
+    n = abs(n)
+    square = n * n
+    
+    temp = n
+    num_of_digits = 0
+    if n == 0: 
+        num_of_digits = 1
+    else:
+        while temp > 0:
+            num_of_digits += 1
+            temp //= 10
+            
+    divisor = 10 ** num_of_digits
+    last_digits = square % divisor
+    
+    return last_digits == n
