8a959660cf3769441283785ff12583117ebc95ee
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 19:12:57 +0530

    Harshad_Number

--- 01-Basic-Programming/Numbers/14_Harshad_Number.py
+++ 01-Basic-Programming/Numbers/14_Harshad_Number.py
@@ -1,1 +1,14 @@
-
+def is_harshad_number(n):
+    if n <= 0:
+        return False
+    
+    original_num = n
+    sum_of_digits = 0
+    
+    temp = n
+    while temp > 0:
+        digit = temp % 10
+        sum_of_digits += digit
+        temp //= 10
+        
+    return original_num % sum_of_digits == 0
