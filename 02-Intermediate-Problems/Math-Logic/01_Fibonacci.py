6046e409b083d272873b23aa748444555cf78a70
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 22:50:49 +0530

    Fibonacci

--- 02-Intermediate-Problems/Math-Logic/01_Fibonacci.py
+++ 02-Intermediate-Problems/Math-Logic/01_Fibonacci.py
@@ -1,1 +1,12 @@
-
+n = int(input())
+if n <= 0:
+    pass 
+elif n == 1:
+    print(0)
+else:
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        temp = a + b
+        a = b
+        b = temp
