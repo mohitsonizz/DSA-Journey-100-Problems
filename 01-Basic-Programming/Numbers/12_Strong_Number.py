6d0b456bfa772719d79808d8ef4ac4064e3fdbd6
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-05 18:33:03 +0530

    Strong_Number

--- 01-Basic-Programming/Numbers/12_Strong_Number.py
+++ 01-Basic-Programming/Numbers/12_Strong_Number.py
@@ -1,1 +1,17 @@
-
+def check_strong_number(n):
+  
+    fact = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+    
+    original_n = n
+    sum_of_facts = 0
+    
+    if n == 0:
+        return False
+        
+    temp = n
+    while temp > 0:
+        digit = temp % 10       
+        sum_of_facts += fact[digit] 
+        temp //= 10               
+        
+    return sum_of_facts == original_n
