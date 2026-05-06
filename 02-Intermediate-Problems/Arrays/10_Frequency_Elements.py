d9e210b2bfe90f10363847d804bf8e4fc2db6f9a
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-06 19:17:44 +0530

    Frequency_Elements

--- 02-Intermediate-Problems/Arrays/10_Frequency_Elements.py
+++ 02-Intermediate-Problems/Arrays/10_Frequency_Elements.py
@@ -1,1 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
 
+def count_frequency(arr):
+    freq = {}
+    for item in arr:
+        if item in freq:
+            freq[item] += 1
+        else:
+            freq[item] = 1
+    
+    return freq
+
+print(count_frequency(arr))
