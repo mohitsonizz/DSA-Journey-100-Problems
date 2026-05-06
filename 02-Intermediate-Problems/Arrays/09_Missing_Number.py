3bc4a9de9cbf27ad90213d6561ee3563e03f3450
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-06 16:41:59 +0530

    Missing_Number

--- 02-Intermediate-Problems/Arrays/09_Missing_Number.py
+++ 02-Intermediate-Problems/Arrays/09_Missing_Number.py
@@ -1,1 +1,10 @@
-
+def find_missing_number():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    expected_sum = n * (n + 1) // 2
+    
+    actual_sum = sum(arr)
+    
+    missing = expected_sum - actual_sum
+    print(missing)
