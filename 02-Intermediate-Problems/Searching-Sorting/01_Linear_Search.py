e164f9142bee0973182b846c5106359e89093414
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-06 20:17:36 +0530

    Linear_Search

--- 02-Intermediate-Problems/Searching-Sorting/01_Linear_Search.py
+++ 02-Intermediate-Problems/Searching-Sorting/01_Linear_Search.py
@@ -1,1 +1,14 @@
+    def linear_search():
+ 
+        n = int(input())
+        arr = list(map(int, input().split()))
+        target = int(input())
 
+        found_index = -1
+        
+        for i in range(n):
+            if arr[i] == target:
+                found_index = i
+                break 
+
+        print(found_index)
