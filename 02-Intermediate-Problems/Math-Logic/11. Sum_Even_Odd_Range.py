0921abc1be004a7f16e8998cd1fc892980b9facb
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-06 09:50:38 +0530

    Sum_Even_Odd_Range

--- 02-Intermediate-Problems/Math-Logic/11. Sum_Even_Odd_Range.py
+++ 02-Intermediate-Problems/Math-Logic/11. Sum_Even_Odd_Range.py
@@ -1,9 +1,18 @@
 def sum_of_even(n):
+     if n < 0: return 0
     k = n // 2
     return k * (k + 1)
 
 def sum_of_odd(n):
     k = (n + 1) // 2
     return k * k
+
+def sum_even_range(L, R):
+    return sum_of_even(R) - sum_of_even(L - 1)
+
+
+def sum_odd_range(L, R):
+    return sum_of_odd(R) - sum_of_odd(L - 1)
+
 
 
