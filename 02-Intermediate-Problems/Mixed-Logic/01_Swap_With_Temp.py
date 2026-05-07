5f1127390eff3e3ae6a21a21036d7230f446bcc7
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 09:04:58 +0530

    Swap_With_Temp

--- 02-Intermediate-Problems/Mixed-Logic/01_Swap_With_Temp.py
+++ 02-Intermediate-Problems/Mixed-Logic/01_Swap_With_Temp.py
@@ -1,1 +1,7 @@
-
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+print(f"Before Swap: a = {a}, b = {b}")
+temp = a
+a = b
+b = temp
+print(f"After Swap: a = {a}, b = {b}")
