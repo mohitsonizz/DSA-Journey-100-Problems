d8c5caede4426abcb2b33983090a379045049d18
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 09:11:31 +0530

    Basic_Calculator

--- 02-Intermediate-Problems/Mixed-Logic/04_Basic_Calculator.py
+++ 02-Intermediate-Problems/Mixed-Logic/04_Basic_Calculator.py
@@ -1,1 +1,9 @@
-
+num1, num2 = 10, 5
+op = '*'
+ops = {
+    '+': num1 + num2,
+    '-': num1 - num2,
+    '*': num1 * num2,
+    '/': num1 / num2 if num2 != 0 else "Error"
+}
+print(ops.get(op, "Invalid Operator"))
