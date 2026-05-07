cc2c5d7ad0baa1da0ffec1bd4269f24efe3ea53b
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 11:26:02 +0530

    Number_To_Words

--- 02-Intermediate-Problems/Math-Logic/15_Number_To_Words.py
+++ 02-Intermediate-Problems/Math-Logic/15_Number_To_Words.py
@@ -1,1 +1,23 @@
+def number_to_words(n):
+    ones = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
+            "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+    tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+    
+    if n == 0:
+        return "Zero"
+    
+    if n < 20:
+        return ones[n]
+    
+    if n < 100:
+        return tens[n // 10] + (" " + ones[n % 10] if (n % 10 != 0) else "")
+    
+    if n < 1000:
+        return ones[n // 100] + " Hundred" + (" and " + number_to_words(n % 100) if (n % 100 != 0) else "")
+    
+    if n < 100000:
+        return number_to_words(n // 1000) + " Thousand" + (" " + number_to_words(n % 1000) if (n % 1000 != 0) else "")
+
+    return "Number too large"
+
 
