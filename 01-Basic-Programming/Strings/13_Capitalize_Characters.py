c84de3e724c8570c37418e2e79c922020d5674ba
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 11:17:11 +0530

    Capitalize_Characters

--- 01-Basic-Programming/Strings/13_Capitalize_Characters.py
+++ 01-Basic-Programming/Strings/13_Capitalize_Characters.py
@@ -1,1 +1,10 @@
+def capitalize_first_letter(sentence):
+    words = sentence.split()
+    result = []
+    
+    for word in words:
+        new_word = word[0].upper() + word[1:].lower()
+        result.append(new_word)
+        
+    return " ".join(result)
 
