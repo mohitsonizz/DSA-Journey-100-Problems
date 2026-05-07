dc97f2447358e26d47f740c42bed2bb3ed09f6e8
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 09:34:47 +0530

    Diagonal_Sum

--- 03-Advanced-DSA/03_Diagonal_Sum.py
+++ 03-Advanced-DSA/03_Diagonal_Sum.py
@@ -1,1 +1,11 @@
+def both_diagonals_sum(n, matrix):
+    primary = 0
+    secondary = 0
+    
+    for i in range(n):
+        primary += matrix[i][i]
+        secondary += matrix[i][n - i - 1]
+        
+    return primary, secondary
+
 
