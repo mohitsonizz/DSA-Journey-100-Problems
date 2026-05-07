f49b0f56f0473ce844b40ada9fa684a5b7b6f0af
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 10:22:52 +0530

    Perfect_Number_In_Range

--- 01-Basic-Programming/Numbers/19_Perfect_Number_In_Range.py
+++ 01-Basic-Programming/Numbers/19_Perfect_Number_In_Range.py
@@ -1,1 +1,21 @@
+import math
+
+def is_perfect(num):
+    if num < 2:
+        return False
+    div_sum = 1
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            div_sum += i
+           
+            if i * i != num:
+                div_sum += num // i         
+    return div_sum == num
+
+def find_perfect_in_range(start, end):
+    result = []
+    for i in range(start, end + 1):
+        if is_perfect(i):
+            result.append(i)
+    return result
 
