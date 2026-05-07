4d49490e56c0c189a3e37036802c3957d8b9fdf6
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 11:39:34 +0530

    Boundary_Traversal

--- 03-Advanced-DSA/07_Boundary_Traversal.py
+++ 03-Advanced-DSA/07_Boundary_Traversal.py
@@ -1,1 +1,31 @@
+def boundary_traversal(matrix):
+    if not matrix:
+        return []
+    
+    rows = len(matrix)
+    cols = len(matrix[0])
+    result = []
+    if rows == 1:
+        return matrix[0]
+    if cols == 1:
+        return [matrix[i][0] for i in range(rows)]
+    for j in range(cols):
+        result.append(matrix[0][j])
+    for i in range(1, rows):
+        result.append(matrix[i][cols - 1])
+    for j in range(cols - 2, -1, -1):
+        result.append(matrix[rows - 1][j])
+    for i in range(rows - 2, 0, -1):
+        result.append(matrix[i][0])
+
+    return result
+
+mat = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+print(*(boundary_traversal(mat)))
 
