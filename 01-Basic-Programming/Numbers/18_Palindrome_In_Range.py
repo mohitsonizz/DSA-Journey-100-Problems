737f782e95c6a8a84935d0f1426a019f4f593e1e
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 10:20:48 +0530

    Palindrome_In_Range

--- 01-Basic-Programming/Numbers/18_Palindrome_In_Range.py
+++ 01-Basic-Programming/Numbers/18_Palindrome_In_Range.py
@@ -1,1 +1,7 @@
+def find_palindromes_fast(start, end):
+    palindromes = []
+    for num in range(start, end + 1):
+        if str(num) == str(num)[::-1]:
+            palindromes.append(num)
+    return palindromes
 
