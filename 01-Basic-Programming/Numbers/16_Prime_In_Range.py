546c8662a42874e88b63cd933ca95b9027f33653
Mohit Kumar <mohitsoni.education@gmail.com>
2026-05-07 10:09:38 +0530

    Prime_In_Range

--- 01-Basic-Programming/Numbers/16_Prime_In_Range.py
+++ 01-Basic-Programming/Numbers/16_Prime_In_Range.py
@@ -1,1 +1,13 @@
+def find_primes_in_range(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if num > 1:
+            is_prime = True
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                primes.append(num)       
+    return primes
 
